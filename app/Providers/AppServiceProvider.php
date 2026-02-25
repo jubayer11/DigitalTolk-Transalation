04ba99ec216bff2d69baa6df3e302325ee9a3e62
Jubayer Ahmed <jubayer@Jubayers-MacBook-Air.local>
2026-02-25 21:46:17 +0600

    chore(container): bind translation repositories and services

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,12 @@
 
 namespace App\Providers;
 
+use App\Contracts\Repositories\TranslationRepositoryInterface;
+use App\Contracts\Services\TranslationExportServiceInterface;
+use App\Contracts\Services\TranslationServiceInterface;
+use App\Repositories\Eloquent\TranslationRepository;
+use App\Services\TranslationExportService;
+use App\Services\TranslationService;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -12,6 +18,10 @@ class AppServiceProvider extends ServiceProvider
     public function register(): void
     {
         //
+        $this->app->bind(TranslationRepositoryInterface::class, TranslationRepository::class);
+        $this->app->bind(TranslationServiceInterface::class, TranslationService::class);
+        $this->app->bind(TranslationExportServiceInterface::class, TranslationExportService::class);
+
     }
 
     /**
